ffb79756e922d5934f08bb17cdfd21aea8760435
binhkid2 <binhkid2@gmail.com>
2023-06-01 20:48:47 +0700

    aa

---
+++ .env.js
@@ -0,0 +1,6 @@
+module.exports = {
+  ...process.env,
+  MONGODB_URI: 'mongodb+srv://binkk03:7uH7mIl5JAggRSF8@cluster0.zymohlw.mongodb.net/?retryWrites=true&w=majority',
+  DOMAIN: 'bisite.click',
+  PORT: 9000  
+};
